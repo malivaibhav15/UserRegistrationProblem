0d48b55f00c269ca7231a2aed437f5abbd98dffc
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-04 10:22:32 +0530

    ADDED : Check first name is valid or not

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
-echo "Welcome to user registration problem"
+echo "Welcome to user registration problem".
+pattern="^[A-Z][a-zA-Z]{2,}$"
+read -p "Enter the name=" name
+if [[ $name =~ $pattern ]]
+then
+	echo "$name is valid name"
+else
+	echo "$name is invalid name"
+fi
