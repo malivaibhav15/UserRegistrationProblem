4a2ca2154670a1ad947d366cc568db9680bd28e6
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-04 12:53:42 +0530

    ADDED : Check email validation

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -22,5 +22,17 @@ function lastName()
 		echo "$name is invalid name"
 	fi
 }
+function eMail()
+{
+	pattern="^[a-z]{1,}([.+-_]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{1,}([.]?[a-z]{2,})?$"
+	read -p "Enter the mail-id=" email
+	if [[ $email =~ $pattern ]]
+	then
+		echo "e-Mail id is valid"
+	else
+		echo "e-Mail id is not valid"
+	fi
+}
 firstName
 lastName
+eMail
