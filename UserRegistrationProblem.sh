034f3bfbdf5e110c2ff73b9f796a8c6c1321a4ca
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-04 10:39:50 +0530

    ADDED : Display last name is valid or not

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
-echo "Welcome to user registration problem".
-pattern="^[A-Z][a-zA-Z]{2,}$"
-read -p "Enter the name=" name
-if [[ $name =~ $pattern ]]
-then
-	echo "$name is valid name"
-else
-	echo "$name is invalid name"
-fi
+echo "Welcome to user registration problem"
+function firstName()
+{
+	pattern="^[A-Z][a-zA-Z]{2,}$"
+	read -p "Enter the first name=" name
+	if [[ $name =~ $pattern ]]
+	then
+		echo "$name is valid name"
+	else
+		echo "$name is invalid name"
+	fi
+}
+function lastName()
+{
+	pattern="^[A-Z][a-zA-Z]{2,}$"
+	read -p "Enter the last name=" name
+	if [[ $name =~ $pattern ]]
+	then
+		echo "$name is valid name"
+	else
+		echo "$name is invalid name"
+	fi
+}
+firstName
+lastName
